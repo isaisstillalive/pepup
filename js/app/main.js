290ae4e0c4b005b7afec67efd276ca7f46355b03
isaisstillalive <isaisstillalive@gmail.com>
2020-03-04 14:29:47 +0900

    パスのミスを修正

--- js/app/main.js
+++ js/app/main.js
@@ -17,7 +17,7 @@ define(function(require) {
       },
       data() {
         return {
-          data: `../mode/${mode}/cell.svg`
+          data: `mode/${mode}/cell.svg`
         };
       },
       mounted() {
