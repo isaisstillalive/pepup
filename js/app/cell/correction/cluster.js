833f666899a4694f4d652010ab63a82cc8e7c7f6
isaisstillalive <isaisstillalive@gmail.com>
2020-03-08 09:05:11 +0900

    塊判定をモジュール化

---
+++ js/app/cell/correction/cluster.js
@@ -0,0 +1,33 @@
+define(function(require) {
+  return (cell, marks = [true]) => {
+    class Module extends cell {
+      get cluster() {
+        return (
+          this.isCluster() ||
+          this.cell(0, -1).isCluster() ||
+          this.cell(-1, 0).isCluster() ||
+          this.cell(-1, -1).isCluster()
+        );
+      }
+
+      isCluster() {
+        if (this.wall || marks.every(mark => this.marked !== mark)) {
+          return false;
+        }
+        const arounds = [
+          [1, 0],
+          [0, 1],
+          [1, 1]
+        ];
+        for (const around of arounds) {
+          const cell = this.cell(...around);
+          if (cell.wall || cell.marked !== this.marked) {
+            return false;
+          }
+        }
+        return true;
+      }
+    }
+    return Module;
+  };
+});
